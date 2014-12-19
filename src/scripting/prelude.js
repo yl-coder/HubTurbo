0c5284d5fa58ae1825e2deed781ea2c68b2298ca
Darius Foo <darius.foo.tw@gmail.com>
2014-12-19 15:01:55 +0800

    More prelude utility functions

--- src/scripting/prelude.js
+++ src/scripting/prelude.js
@@ -4,3 +4,66 @@ var ScriptManager = Java.type('scripting.ScriptManager');
 var alert = ScriptManager.alert;
 var scriptError = ScriptManager.scriptError;
 
+var filter = function (pred, list) {
+    if (list.filter) {
+        return list.filter(pred);
+    } else {
+        var result = [];
+        for (var i=0; i<list.length; i++) {
+            if (pred(list[i])) {
+               result.push(list[i]);
+            }
+        }
+        return result;
+    }
+};
+
+var map = function (fn, list) {
+    if (list.map) {
+        return list.map(fn);
+    } else {
+        var result = [];
+        for (var i=0; i<list.length; i++) {
+            result.push(fn(list[i], i));
+        }
+        return result;
+    }
+};
+
+var reduce = function (op, initial, list) {
+    if (list.reduce) {
+        return list.reduce(op, initial);
+    } else {
+        var result = initial;
+        for (var i=0; i<list.length; i++) {
+            result = op(result, list[i]);
+        }
+        return result;
+    }
+};
+
+Array.prototype.toString = function () {
+    var sb = [];
+    for (var i=0; i<this.length-1; i++) {
+        sb.push(this[i] + ',');
+    }
+    sb.push(this[this.length-1]);
+    return '[' + sb.join(' ') + ']';
+};
+
+Object.prototype.toString = function () {
+    var sb = [];
+    Object.keys(this).forEach(function (key) {
+        sb.push(key + ": " + this[key]);
+    });
+    return '{' + sb.join(', ') + '}';
+};
+
+function list(xs) {
+    var result = [];
+    for (var i=0; i<xs.length; i++) {
+        result.push(xs[i]);
+    }
+    return result;
+}
+
