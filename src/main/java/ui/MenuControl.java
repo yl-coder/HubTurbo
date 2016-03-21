c791da816ce245f8d3b5ec0b5ae09133dae29204
You Liang Lim <a0125975@u.nus.edu>
2016-03-21 17:09:25 -0400

    Renamed file menu File -> App

--- src/main/java/ui/MenuControl.java
+++ src/main/java/ui/MenuControl.java
@@ -67,7 +67,8 @@ public class MenuControl extends MenuBar {
     }
 
     private Menu createFileMenu() {
-        Menu file = new Menu("File");
+
+        Menu file = new Menu("App");
 
         MenuItem logout = new MenuItem("Logout");
         logout.setOnAction(e -> {
